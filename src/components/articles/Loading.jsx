49ffe5db7ab8c6efa5f97d1316bcb4b2cb84c17e
Thomas Stussi <tstussi06@gmail.com>
2020-10-21 16:17:36 -0700

    loading screen

---
+++ src/components/articles/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loading = () => (
+  <img
+    src="https://www.placecage.com/gif/284/196"
+    alt="placecage gif"
+  />
+);
+
+export default Loading;
--- src/components/container/NewsSearch.jsx
+++ src/components/container/NewsSearch.jsx
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 import ArticleList from '../articles/ArticleList';
 import Search from '../search/Search';
+import Loading from '../articles/Loading';
 import { getArticles } from '../../services/news-api';
 
 export default class AllArticles extends Component {
   state = {
     articles: [],
-    search: ''
+    search: '',
+    loading: true
   }
 
   handleChange = ({ target }) => {
@@ -17,11 +19,21 @@ export default class AllArticles extends Component {
   handleSearch = async () => {
     const articleArray = await getArticles(this.state.search);
 
-    this.setState({ articles: articleArray });
+    this.setState({ articles: articleArray, loading: false });
   }
 
   render() {
-    const { articles, search } = this.state;
+    const { articles, search, loading } = this.state;
+
+    if (loading) return <>
+      <Search
+        search={search}
+        onSubmit={this.handleSearch}
+        onChange={this.handleChange}
+      />
+      <br />
+      <Loading />
+    </>;
     return (
       <>
         <Search
